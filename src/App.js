a2859d74b6d334bdd6ea128eb92c5b023f076328
Sean Charoutanh <scharoutanh@gmail.com>
2021-08-26 16:15:46 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/client';
+import { PokemonsContainer } from './containers/PokemonContainer';
+
+function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon2.vercel.app/'
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
+  );
+}
+
+export default App
